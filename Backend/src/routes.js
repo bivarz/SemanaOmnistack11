8eba6863b4064af4b5ff27d2cf92fa07f4c2eb79
bivarz <paulo.bivar@gmail.com>
2020-03-28 16:18:20 -0300

    Atualizando

--- Backend/src/routes.js
+++ Backend/src/routes.js
@@ -1,9 +1,20 @@
 import { Router } from 'express';
 import OngController from './controllers/OngController';
+import IncidentController from './controllers/IncidentController';
+import ProfileController from './controllers/ProfileController';
+import SessionController from './controllers/SessionController';
 
 const routes = new Router();
 
+routes.post('/sessions', SessionController.store);
+
 routes.post('/ongs', OngController.store);
 routes.get('/ongs', OngController.index);
+
+routes.get('/profile', ProfileController.index);
+
+routes.post('/incidents', IncidentController.store);
+routes.get('/incidents', IncidentController.index);
+routes.delete('/incidents/:id', IncidentController.delete);
 
 export default routes;
